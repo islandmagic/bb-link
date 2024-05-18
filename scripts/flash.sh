ac786acd13d68fa5014776cbf9c0eb59cd80fa6f
Georges Auberger <3819+georges@users.noreply.github.com>
2024-05-17 14:55:32 -1000

    Flashing script

---
+++ scripts/flash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "*******************************************************"
+echo "*** Flashing BB-Link to all connected ESP32 devices ***"
+echo "*******************************************************"
+
+devices=$(arduino-cli board list | grep 'wchusbserial' | awk '{print $1}')
+for device in $devices; do
+  echo "Flashing BB-Link to $device"
+  arduino-cli board attach -b esp32:esp32:pico32 -p $device ../src/bb-link/bb-link.ino
+  arduino-cli compile -u ../src/bb-link/bb-link.ino
+done
+echo "*******************************************************"
+echo "*** Done                                            ***"
+echo "*******************************************************"
